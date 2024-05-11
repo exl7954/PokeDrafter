94dc81db07b4305babaaa830647c7e5b6001ee57
exl7954 <exl7954@nyu.edu>
2024-05-11 09:24:47 -0400

    more user tests

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -37,6 +37,37 @@ async def test_login(client):
     assert response.json()["access_token"]
 
 @pytest.mark.anyio
+async def test_get_users(client):
+    '''
+    Test getting users.
+    '''
+    response = await client.get("/users/")
+    assert response.status_code == 200
+    assert response.json()["users"]
+
+@pytest.mark.anyio
+async def test_get_user(client):
+    '''
+    Test getting a user.
+    '''
+    response = await client.get(f"/users/id/{user_dict['pytestuser']}")
+    assert response.status_code == 200
+    assert response.json()["username"] == "pytestuser"
+    assert "email" not in response.json()
+    assert "password" not in response.json()
+
+@pytest.mark.anyio
+async def test_get_me(client):
+    '''
+    Test getting the current user.
+    '''
+    response = await client.get("/users/me",
+                                headers={"Authorization": f"Bearer {token_dict['pytestuser']}"})
+    assert response.status_code == 200
+    assert response.json()["username"] == "pytestuser"
+
+
+@pytest.mark.anyio
 async def test_delete_user(client):
     '''
     Test deleting a user.

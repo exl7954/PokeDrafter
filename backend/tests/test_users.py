680aab53bb293c36eaffc609370b1c71354f4e4b
exl7954 <exl7954@nyu.edu>
2024-05-11 09:06:44 -0400

    fix tests

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,32 +1,19 @@
 '''
 Unit tests for the user functionalities.
 '''
+import pytest
 from fastapi.testclient import TestClient
 from backend.app import app
 
-client = TestClient(app)
 user_dict = {}
 token_dict = {}
 
-
-def test_login():
-    '''
-    Test logging in a user.
-    '''
-    username = "testuser"
-    password = "testpass"
-    response = client.post("/token",
-                            headers={"Content-Type": "application/x-www-form-urlencoded"},
-                            data={"username": username, "password": password})
-    token_dict[username] = response.json()["access_token"]
-    assert response.status_code == 200
-    assert response.json()["access_token"]
-
-def test_create_user():
+@pytest.mark.anyio
+async def test_create_user(client):
     '''
     Test creating a user.
     '''
-    response = client.post("/users/create", json={
+    response = await client.post("/users/create", json={
         "username": "pytestuser",
         "email": "pytest@test.com",
         "password": "testpassword"
@@ -35,10 +22,25 @@ def test_create_user():
     assert response.status_code == 201
     assert response.json()["username"] == "pytestuser"
 
-# def test_delete_user():
-#     '''
-#     Test deleting a user.
-#     '''
-#     response = client.delete("/users/delete", headers={"Authorization": f"Bearer {token_dict['pytestuser']}"})
-#     assert response.status_code == 200
-#     assert response.json()["message"] == "User deleted."
+@pytest.mark.anyio
+async def test_login(client):
+    '''
+    Test logging in a user.
+    '''
+    username = "pytestuser"
+    password = "testpassword"
+    response = await client.post("/token",
+                            headers={"Content-Type": "application/x-www-form-urlencoded"},
+                            data={"username": username, "password": password})
+    token_dict[username] = response.json()["access_token"]
+    assert response.status_code == 200
+    assert response.json()["access_token"]
+
+@pytest.mark.anyio
+async def test_delete_user(client):
+    '''
+    Test deleting a user.
+    '''
+    response = await client.delete("/users/delete", headers={"Authorization": f"Bearer {token_dict['pytestuser']}"})
+    assert response.status_code == 200
+    assert response.json()["message"] == "User deleted."
